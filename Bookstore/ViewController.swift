8bbaa4f9efbbc1a216589d7a7e26de45e57f2a19
Eddy Fidel <eddy.fidel0809@gmail.com>
2018-10-17 00:20:08 -0400

    Initial Commit

---
+++ Bookstore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bookstore
+//
+//  Created by Eddy Fidel on 10/17/18.
+//  Copyright © 2018 Eddy Fidel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
